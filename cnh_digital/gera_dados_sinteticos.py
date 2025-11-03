c9c0a823367260c2157da8c6547b7082c6b15d40
Wagyla <wsm12@aluno.ifnmg.edu.br>
2025-11-03 15:32:44 -0300

    Posições dos campos da CNH

--- cnh_digital/gera_dados_sinteticos.py
+++ cnh_digital/gera_dados_sinteticos.py
@@ -9,7 +9,7 @@ def gerar_csv(qtd_itens, nome_arquivo="dados_fakes.csv"):
     with open(nome_arquivo, mode='w', newline='', encoding='utf-8') as arquivo:
         escritor = csv.writer(arquivo)
         escritor.writerow(["nome_completo", "prim_habi", "data_nasci", "local_nasci", "uf_3", "data_emissao", 
-            "validade", "doc_identida", "org_emissor", "uf_4c", "cpf", "num_regis", "categoria", "filiacao", "assinatura"])
+            "validade", "doc_identida", "org_emissor", "uf_4c", "cpf", "num_regis", "categoria", "pai", "mae", "assinatura"])
         for _ in range(qtd_itens):
             nome = fake.name()
             tipo = random.choice(categoria_hab)
